0fd983951104d28a5dc3f8a492ef918714f432a7
Zuka <marcjacoblasam123@gmail.com>
2024-04-04 18:12:22 +0800

    refactor: change the overall code

--- script.js
+++ script.js
@@ -1,48 +1,84 @@
-const grid = document.querySelector('.grid-container');
-const showSize = document.querySelector('.js-size-value');
-const sizeSlider = document.querySelector('#js-size-slider');
+const color = document.querySelector('#js-color-pick');
+const size = document.querySelector('#js-size-slider');
+const sizeVal = document.querySelector('.js-size-value');
+const grid = document.querySelector('#js-grid-container');
+const rainbow = document.querySelector('#js-rainbow-color');
+const eraser = document.querySelector('#js-eraser');
+const clear = document.querySelector('#js-clear-grid');
 
+let currentSize = 16;
+let currentColor = '#000000';
+let toggleRainbow = false;
+let toggleEraser = false;
 
-let size = 16;
-//get size
-sizeSlider.addEventListener('mouseup', (event) => {
-    
-    size = event.target.value;
-    console.log(size);
-    createGrid(size);
-    
+//create the default size grid 16x16
+createGrid(currentSize);
+
+//Event Listener
+size.addEventListener('mouseup', (event) => {
+  let size = getSizeValue(event.target.value);
+  createGrid(size);
 });
 
-//show size
-sizeSlider.addEventListener('input', (event) => {
-    showSize.textContent = `Size: ${event.target.value}`; 
+size.addEventListener('input', (event) => updateSizeValue(event.target.value));
+
+rainbow.addEventListener('click', () => {
+  toggleEraser = false;
+  toggleRainbow = true;
 });
 
-//create grid
+eraser.addEventListener('click', () => {
+  toggleRainbow = false;
+  toggleEraser = true;
+});
+
+clear.addEventListener('click', () => resetGrid());
+
+color.addEventListener('change', (event) => {
+  toggleEraser = false;
+  toggleRainbow = false;
+  currentColor = event.target.value;
+});
+
+//functions
+function getSizeValue(newSize) {
+  currentSize = newSize;
+
+  return currentSize;
+}
+
+function updateSizeValue(size) {
+  return (sizeVal.textContent = `Size: ${size} x ${size}`);
+}
+
 function createGrid(size) {
-    for (let i = 0; i < size; i++) {
-        let row = document.createElement('div');
-        row.classList.add('grid-item');
-        grid.appendChild(row);
-        for(let j = 0; j < size; j++) {
-            let col = document.createElement('div');
-            let currentColor = changeColor();
-            col.classList.add('grid-item-sub');
-            col.addEventListener('mousedown', () => {
-                col.style.backgroundColor = currentColor;
-            })
-            row.appendChild(col);
+  grid.innerHTML = '';
+  for (let i = 0; i < size; i++) {
+    let row = document.createElement('div');
+    row.classList.add('grid-item-row');
+    for (let j = 0; j < size; j++) {
+      let col = document.createElement('div');
+      col.classList.add('grid-item-col');
+      col.addEventListener('mouseover', () => {
+        if (toggleRainbow === true) {
+          col.style.backgroundColor = generateRainbow();
+        } else if (toggleEraser === true) {
+          col.style.backgroundColor = '#fff';
+        } else {
+          col.style.backgroundColor = currentColor;
         }
+      });
+      row.appendChild(col);
     }
+    grid.appendChild(row);
+  }
 }
 
-function changeColor() {
-    document.querySelector('#js-color-pick').addEventListener('change', (event) => {
-        let color = event.target.value;
-
-        return color;
-    })
+function resetGrid() {
+  createGrid(currentSize);
 }
 
-
-
+function generateRainbow() {
+  let randomColor = '#' + (Math.random().toString(16) + '000000').substring(2, 8);
+  return randomColor;
+}
